f17714274f4b2f3992720bbd776e29edb087809d
kaig2008 <kaig2008@126.com>
2016-09-29 10:08:26 +0800

    添加ico

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,31 +1,37 @@
 //index.js
 //获取应用实例
 var app = getApp()
-Page({
+Page( {
   data: {
     motto: 'Hello World-宝儿',
     userInfo: {},
-    cat:{name:'猫的图片'}
+    cat: { name: '猫的图片' }
   },
   //事件处理函数
   bindLogViewTap: function() {
-    wx.navigateTo({
+    wx.navigateTo( {
       url: '../logs/logs'
     })
   },
-   bindCatViewTap: function() {
-    wx.navigateTo({
+  bindCatViewTap: function() {
+    wx.navigateTo( {
       url: '../cat/cat'
     })
   },
-  onLoad: function () {
-    console.log('onLoad')
+  bindViewTap: function(event) {
+    var pageName=event.currentTarget.dataset.pageName;
+    var _url='../{page}/{page}'.replace('{page}',pageName).replace('{page}',pageName);
+    console.log(_url);
+    wx.navigateTo( { url: _url });
+  },
+  onLoad: function() {
+    console.log( 'onLoad' )
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+    app.getUserInfo( function( userInfo ) {
       //更新数据
-      that.setData({
-        userInfo:userInfo
+      that.setData( {
+        userInfo: userInfo
       })
     })
   }
